358806994ce29791877f02720ec9ee0c95f2ae89
all0s <114631691+all0s@users.noreply.github.com>
2022-10-05 19:38:52 +0800

    Add `Layout` component and `breakpoints.scss`

---
+++ src/components/common/Layout/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import styles from './Layout.module.scss'
+
+export type LayoutProps = {
+  children?: ReactNode
+}
+
+function Layout({ children }: LayoutProps) {
+  return <div className={styles.container}>{children}</div>
+}
+
+export default Layout
